216669a153a19539532918bce32d68c2836c9757
VicSimoes <victor.simoes@gmail.com>
2014-02-09 15:05:51 -0600

    Finished Homework

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20140209152128) do
+
+  create_table "movies", force: true do |t|
+    t.string   "title"
+    t.integer  "year"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "products", force: true do |t|
+    t.string   "name"
+    t.string   "image_url"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
